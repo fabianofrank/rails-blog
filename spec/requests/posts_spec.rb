0004cdd957dc718114497e5b18fcb0c4d5194006
fabianofrank <fabianofrank@protonmail.com>
2022-08-15 22:07:16 -0300

    Add show specs for users and posts

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
-    before(:each) { get '/users/26/posts' } # get(:users)
+    before(:each) { get '/users/:id/posts' } # get(:users)
 
     it 'returns response status success' do
       expect(response).to have_http_status(:ok)
@@ -13,7 +13,23 @@ RSpec.describe 'Posts', type: :request do
     end
 
     it 'shows placeholder text success' do
-      expect(response.body).to include('head')
+      expect(response.body).to render_template('index')
+    end
+  end
+
+  describe 'GET /show' do
+    before(:each) { get '/users/:id/posts/:id' } # get(:users)
+
+    it 'returns response status success' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'renders template success' do
+      expect(response.body).to match(/body/)
+    end
+
+    it 'shows placeholder text success' do
+      expect(response.body).to render_template('show')
     end
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -13,7 +13,23 @@ RSpec.describe 'Users', type: :request do
     end
 
     it 'shows placeholder text success' do
-      expect(response.body).to include('Frank')
+      expect(response.body).to render_template('index')
+    end
+  end
+
+  describe 'GET /show' do
+    before(:example) { get '/users/:id' } # get(:users)
+
+    it 'returns response status success' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'renders template success' do
+      expect(response.body).to match(/Frank/)
+    end
+
+    it 'shows placeholder text success' do
+      expect(response.body).to render_template('show')
     end
   end
 end
