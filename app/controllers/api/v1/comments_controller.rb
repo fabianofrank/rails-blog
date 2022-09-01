0bf1e5afc36fbf8211ff9a35004d0cabe0ce042d
fabianofrank <fabianofrank@protonmail.com>
2022-09-01 20:47:24 -0300

    Add new API routes

---
+++ app/controllers/api/v1/comments_controller.rb
@@ -0,0 +1,23 @@
+class Api::V1::CommentsController < ApplicationController
+  def index
+    @post = Post.find(params[:post_id])
+    @comments = @post.comments
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.user_id = params[:user_id]
+    @comment.post_id = params[:post_id]
+    if @comment.save
+      render json: @comment, status: :created
+    else
+      render json: @comment.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,21 @@
 Rails.application.routes.draw do
   devise_for :users
+  
   root to: "users#index"
-
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create, :destroy] do
       resources :comments, only: [:new, :create, :destroy]
       resources :likes, only: [:create, :destroy]
+    end
+  end
+
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [] do
+        resources :posts, only: [:index] do
+          resources :comments, only: [:index, :create]
+        end
+      end
     end
   end
 end
